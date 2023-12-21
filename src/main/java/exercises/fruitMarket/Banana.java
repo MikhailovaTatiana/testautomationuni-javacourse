a7ff8a8dc17065518a7205d92dc8793248b06f53
Tatiana Mikhailova <imagetata@gmail.com>
2023-12-21 18:43:24 +0100

    excerces chapter 10

---
+++ src/main/java/exercises/fruitMarket/Banana.java
@@ -0,0 +1,16 @@
+package exercises.fruitMarket;
+
+public class Banana extends Fruit {
+    public void peel() {
+        System.out.println("Banana has been peeled.");
+    }
+
+    public Banana() {
+        setCalories(120);
+    }
+
+    @Override
+    public void makeJuice() {
+        System.out.println("Banana smoothie is made.");
+    }
+}
---
+++ src/main/java/exercises/fruitMarket/Market.java
@@ -0,0 +1,30 @@
+package exercises.fruitMarket;
+
+public class Market {
+    public static void main(String[] args) {
+        Fruit yellowFruit = new Apple();
+        ((Apple) yellowFruit).removeSeeds();
+        yellowFruit.makeJuice();
+        System.out.println(yellowFruit.getCalories() + " calories");
+
+        System.out.println("===============");
+
+        yellowFruit = new Banana();
+        ((Banana) yellowFruit).peel();
+        yellowFruit.makeJuice();
+        yellowFruit.getCalories();
+        System.out.println(yellowFruit.getCalories() + " calories");
+
+        System.out.println("===============");
+
+        Apple apple = new Apple();
+        muss(apple);
+        Banana banana = new Banana();
+        muss(banana);
+    }
+
+    public static void muss(Fruit fruit) {
+        System.out.println("Zhuuu");
+        fruit.makeJuice();
+    }
+}
